3394178ecd40ed3a34cc435598bb523cd2d82d2a
HexRabbit <mikuqb8718@gmail.com>
2019-02-24 21:16:01 +0800

    Implement queue

--- queue.c
+++ queue.c
@@ -26,7 +26,11 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q) {
+        q->head = NULL;
+        q->size = 0;
+        q->tail = NULL;
+    }
     return q;
 }
 
@@ -35,6 +39,15 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (!q)
+        return;
+    for (list_ele_t *prev, *now = q->head; now != NULL;) {
+        free(now->value);
+        prev = now;
+        now = now->next;
+        free(prev);
+    }
+
     free(q);
 }
 
@@ -48,12 +61,32 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
+    char *news;
+
+    if (!q)
+        return false;
     /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+    newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+
+    news = malloc(strlen(s) + 1);
+    if (!news) {
+        free(newh);
+        return false;
+    }
+
+    if (!q->head)
+        q->tail = newh;
     newh->next = q->head;
+    newh->value = news;
     q->head = newh;
+    strcpy(news, s);
+
+    q->size++;
+
     return true;
 }
 
@@ -69,7 +102,35 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    list_ele_t *newh;
+    char *news;
+
+    if (!q)
+        return false;
+
+    newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+
+    news = malloc(strlen(s) + 1);
+    if (!news) {
+        free(newh);
+        return false;
+    }
+
+    if (!q->tail) {
+        q->head = newh;
+    } else {
+        q->tail->next = newh;
+    }
+    q->tail = newh;
+    newh->next = NULL;
+    newh->value = news;
+    strcpy(news, s);
+
+    q->size++;
+
+    return true;
 }
 
 /*
@@ -82,8 +143,25 @@ bool q_insert_tail(queue_t *q, char *s)
 */
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
-    /* You need to fix up this code. */
+    list_ele_t *tmp;
+    if (!q || !q->head)
+        return false;
+
+    if (sp) {
+        strncpy(sp, q->head->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    free(q->head->value);
+    tmp = q->head;
     q->head = q->head->next;
+    free(tmp);
+
+    if (!q->head)
+        q->tail = NULL;
+
+    q->size--;
+
     return true;
 }
 
@@ -95,7 +173,10 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (!q)
+        return 0;
+
+    return q->size;
 }
 
 /*
@@ -107,5 +188,22 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* You need to write the code for this function */
+    if (!q)
+        return;
+
+    list_ele_t *now = q->head;
+    list_ele_t *prev = NULL;
+    list_ele_t *next;
+    list_ele_t *tmp;
+
+    tmp = q->head;
+    q->head = q->tail;
+    q->tail = tmp;
+
+    while (now) {
+        next = now->next;
+        now->next = prev;
+        prev = now;
+        now = next;
+    }
 }
--- queue.h
+++ queue.h
@@ -12,6 +12,7 @@
  */
 
 #include <stdbool.h>
+#include <stdint.h>
 
 /************** Data structure declarations ****************/
 
@@ -26,10 +27,8 @@ typedef struct ELE {
 /* Queue structure */
 typedef struct {
     list_ele_t *head; /* Linked list of elements */
-                      /*
-                        You will need to add more fields to this structure
-                        to efficiently implement q_size and q_insert_tail
-                      */
+    list_ele_t *tail;
+    uint64_t size;
 } queue_t;
 
 /************** Operations on queue ************************/
